103929d7461d5081c9fbbf5ca3473d96da3b0535
codertylerasai@gmail.com <codertylerasai@gmail.com>
2021-01-28 01:03:13 -0700

    different types of formatting

--- src/print.rs
+++ src/print.rs
@@ -1,4 +1,13 @@
 pub fn run() {
   //Print to console
   println!("hello from the print.rs file");
+
+  //Basic formatting
+  println!("Number: {}", 1);
+
+  //Positional arguments
+  println!("{0} is from {1} and {0} likes to {2}", "Tyler", "LHL", "code" );
+
+  //Named arguments
+  println!("{name} likes to play {activity}", name="Tyler", activity="ping pong")
 }
